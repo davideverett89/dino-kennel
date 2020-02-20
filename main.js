2263fa76fb80735ecab91fbd3b35a697e6e519d9
David Everett <davideverett1989@gmail.com>
2020-02-20 16:45:09 -0600

    wip

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
-console.log("Dino Kennel!");
+const dinos = [
+    {
+        id: `dino${dinos.length + 1}`,
+        name: 'Rex',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Zoe',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    },
+    {
+        id: `dino${dinos.length + 1}`,
+        name: 'Rex',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Zoe',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    },
+    {
+        id: `dino${dinos.length + 1}`,
+        name: 'Rex',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Zoe',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    },
+    {
+        id: `dino${dinos.length + 1}`,
+        name: 'Rex',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Zoe',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    },
+    {
+        id: `dino${dinos.length + 1}`,
+        name: 'Rex',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Zoe',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    },
+    {
+        id: `dino${dinos.length + 1}`,
+        name: 'Rex',
+        type: 'T Rex',
+        age: 100,
+        owner: 'Zoe',
+        adventures: [],
+        health: 100,
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    }
+];
